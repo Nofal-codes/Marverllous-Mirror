34bacf49868cd655c32b8074c91caa9aa0a10d80
Nofal-codes <nofal.ahmad@outlook.com>
2017-12-01 20:46:08 +0400

    first test

--- src/App.js
+++ src/App.js
@@ -24,12 +24,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React m8</h1>
+          <h1 className="App-title">React m8</h1>
           <h1 className="App-title">{ this.state.Myname }</h1>
         </header>
         <h1> { this.state.time } </h1>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          lol <code>src/App.js</code> m8
         </p>
         <p className="App-intro">
           Ahmad Nofal App
